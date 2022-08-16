a870b5fdb472f2d6d390dd5f247a3c787ac00880
agar rajak <agarrajak858@gmail.com>
2022-08-16 21:23:15 +0530

    added pagination fix

--- src/common/paginated.component.tsx
+++ src/common/paginated.component.tsx
@@ -1,48 +1,66 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { PagedQueryInterface } from '../config/urls.config';
 import { PagedResponse } from './interfaces/pagedResponse.interface';
 import { InView } from 'react-intersection-observer';
 import Loader from './loader.component';
 
 export interface PaginatedComponentProps<T> {
-    children: (data: T[]) => JSX.Element | JSX.Element[];
+    children: (data: T[], firstElementId: number) => JSX.Element | JSX.Element[];
     apiFunction: (value: PagedQueryInterface) => string;
     uniqueKey: string;
     limit: number;
 }
 
-export default function PaginatedComponent<T>(props: PaginatedComponentProps<T>) {
+export default function PaginatedComponent<T extends { id: number }>(props: PaginatedComponentProps<T>) {
     const { apiFunction, uniqueKey, limit } = props;
 
     const [page, setPage] = useState<number>(1);
+    const firstElementId = useRef<number>(0); // use for moving focus of browser scroll
+    const isInView = useRef<boolean>(false);
+
     const pagedData = useRef<PagedResponse<T>>({
       data: [],
       isNextAvaible: true,
       ok: true,
     });
 
-    const { isLoading, error, data } = useQuery<unknown, unknown, PagedResponse<T>>([uniqueKey, page], () =>
-    fetch(apiFunction({ page, limit}))
-      .then(res => res.json())
-      .then(data => {
-        if (data.ok) {
-          pagedData.current.isNextAvaible = data.isNextAvaible;
-          pagedData.current.data = [...pagedData.current.data, ...data.data];
-          return Promise.resolve(pagedData.current);
+    const { isLoading, error, data } = useQuery<unknown, unknown, PagedResponse<T>>([uniqueKey, page], () => fetch(apiFunction({ page, limit}))
+    .then(res => res.json())
+    .then(data => {
+      if (data.ok) {
+        if (data.data && data.data.length) {
+          firstElementId.current =  data.data[0].id;
+        } else {
+          firstElementId.current = 0;
         }
-        return Promise.reject(data);
-      }));
+        pagedData.current.isNextAvaible = data.isNextAvaible;
+        pagedData.current.data = [...pagedData.current.data, ...data.data];
+        return Promise.resolve(pagedData.current);
+      }
+      return Promise.reject(data);
+    })
+   , {
+        onSuccess: (data) => {
+          // console.log(pagedData.current.isNextAvaible);
+          setTimeout(() => {  // check view is still visible update view 
+            if (isInView.current && data && data.isNextAvaible) {
+              setPage(page+1);
+            }
+          }, 100);
+        },
+      });
 
     const retrigerPageApi = useCallback((InView: boolean) => {
-        if (!isLoading && InView && data && data.isNextAvaible) {
+        isInView.current = InView;
+        if (!isLoading && InView && data &&  data.isNextAvaible) {
           setPage(page + 1);
         }
-    }, [isLoading, data, page]);
+    }, [isLoading, page, data]);
 
   return (
     <div className='flex flex-col w-100'>
-      {props.children(data?.data || [])}
+      {props.children(data?.data || [], firstElementId.current)}
       <InView onChange={retrigerPageApi} threshold={1}>
         <div className='w-100 flex flex-row justify-center'>
           {data && data.isNextAvaible && <Loader />}
--- src/users/components/UserList.component.tsx
+++ src/users/components/UserList.component.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router';
 import PaginatedComponent from '../../common/paginated.component';
 import { CreateUserRoute } from '../../config/routes.config';
@@ -8,25 +9,39 @@ import UserCardComponent from './UserCard.component';
 
 export default function UserListComponent() {
   const navigate = useNavigate();
+  const firstElementRef = useRef<HTMLDivElement>(null);
+ 
   return (
     <PaginatedComponent<UserInterface>
       apiFunction={(value: PagedQueryInterface) => {
         return getUserPagedUrl(value);
       }
-      } uniqueKey={'user/pages'} limit={20}
+      } uniqueKey={'user/pages'} limit={10}
     >
-      {(data) => (
+      {(data, firstElementId) =>  {
+      //  eslint-disable-next-line react-hooks/rules-of-hooks
+        useEffect(() => {
+          if (firstElementId > 0) {
+            firstElementRef.current?.scrollIntoView();
+          }
+        });
+      
+        return (
         <div className='w-100 grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5' >
           {(data || []).map(user =>
-            <div className='mx-5 my-5' key={user.id} onClick={() => navigate(`${user.id}/posts`)}>
-              <UserCardComponent user={user} />
-            </div>
+            {
+              const refProsp = firstElementId === user.id ? { ref: firstElementRef} : {};
+
+              return (<div className='mx-5 my-5' key={user.id} onClick={() => navigate(`${user.id}/posts`)} {...refProsp}>
+                      <UserCardComponent user={user} />
+                </div>)
+            }
           )}
           <div className='mx-5 my-2' onClick={() => navigate(CreateUserRoute())}>
             <AddUserCardComponent />
           </div>
         </div>
-      )}
+      )}}
     </PaginatedComponent>
   );
 }
